04e35748b5aeaecd745a58b648eb98c5a889a5cf
ShabareshReddy <chandrareddy85580@gmail.com>
2025-07-29 10:58:18 +0530

    .env and .env.example

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+require("dotenv").config();
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on the port ${process.env.PORT || 3000}`)
+})
