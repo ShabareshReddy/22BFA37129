87178dc2a24e7511ebd582bc1d571a95dfd42d30
ShabareshReddy <chandrareddy85580@gmail.com>
2025-07-29 12:23:53 +0530

    login config with jwt token sign

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 const userRouter=express.Router();
 const axios=require("axios");
+const jwt=require("jsonwebtoken");
+require("dotenv").config();
+
 
 app.use(express.json());
 
@@ -12,12 +15,37 @@ userRouter.post("/register",async(req,res)=>{
             email,name,mobileNo,githubUserName,rollNo, accessCode
         });
         return res.send({
-            msg:"Your are signedIn succesfully"
+            msg:"You can register only once Do not forget to save Your clientID and clientSecret;you cannot retrive them again"
+           
         });
     }catch(error){
-        res.json({
-           msg: `${error.message}`
-        })
+    res.status(error.response?.status || 500).json({
+        msg: error.message,
+        data: error.response?.data || null
+    });
+}
+});
+
+userRouter.post("/login",async(req,res)=>{
+    try{
+        const{email, name,rollNo,accessCode,clientID,clientSecret }=req.body;
+        const login=await axios.post("http://20.244.56.144/evaluation-service/auth",{
+            email, name,rollNo,accessCode,clientID,clientSecret
+        });
+        if (login.data && login.data.success) {
+            const token = jwt.sign({ email }, process.env.JWT_SECRET);
+            return res.json({
+                msg: 'Login successful',
+                token
+            });
+        } else {
+            return res.status(401).json({ msg: 'Login failed'});
+        }
+    } catch (error) {
+        res.status(error.response?.status || 500).json({
+            msg: error.message,
+            data: error.response?.data || null
+        });
     }
 })
 
