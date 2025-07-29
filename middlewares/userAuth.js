df198103633915971dd9c162723fc035618b10c4
ShabareshReddy <chandrareddy85580@gmail.com>
2025-07-29 12:41:18 +0530

    config token from headers and config with jwtverify

---
+++ middlewares/userAuth.js
@@ -0,0 +1,20 @@
+
+const jwt=require("jsonwebtoken");
+const { JWT_USER_SECRET } = require("../config");
+
+const userAuth=async(req,res,next)=>{
+    const token=req.headers.token;
+    const decoded=await jwt.verify(token,JWT_USER_SECRET);
+    if(decoded){
+        req.userId=decoded.id;
+        next();
+    }else{
+        res.status(401).json({
+            msg:"Unauthorized access"
+        })
+    }
+}
+
+module.exports={
+    userauth:userAuth
+}
--- routes/user.js
+++ routes/user.js
@@ -53,4 +53,6 @@ userRouter.post("/login",async(req,res)=>{
 
 
 
+
+
 module.exports=userRouter
